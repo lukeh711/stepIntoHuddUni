05f93d59452c8366b7092dd67aa768e962597bff
Luke <lukehatherly08@gmail.com>
2026-06-24 11:04:11 +0100

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -1,1 +1,10 @@
-pass
+import requests
+
+i=1
+response = requests.get(f"https://j-archive.com/showseason.php?season={i}")
+
+i=2
+response = requests.get(f"https://j-archive.com/showseason.php?season={i}")
+
+html_data = response.text
+print(html_data)
